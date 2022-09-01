525d188fbfef6adff3c0016417c264a048155fd3
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-01 12:49:08 +0600

    Update home.blade.php and add link to view product list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Laravel 8 Simple CURD Project</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <button class="btn btn-info"><a href="{{route('products.show')}}">Products</a></button>
                 </div>
             </div>
         </div>
